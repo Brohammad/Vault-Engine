5f8e5b666de24327784308be3002dbf90da00695
Brohammad <aabidmohamed2003@gmail.com>
2026-06-24 21:43:23 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('Database connected successfully');
+    }
+    catch{
+        console.log('Error connecting to Database');
+        process.exit(1);
+    }
+}
+modules.exports=connectDB;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express= require('express');
+require('dotenv').config();
+const port=process.env.PORT || 3000;
+const connectDB=require('./config/db');
+const authRoutes=require('./routes/auth-routes')
+
+const app=express();
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+connectDB();
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
